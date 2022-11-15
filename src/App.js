3160b0d37f0dc81f62532a667ecb31fb2c100650
daudi13 <davouma13@gmail.com>
2022-11-15 19:30:41 +0300

    Add useStyle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import Homepage from './Pages/Homepage';
+import Coin from './Pages/Coin';
+import { makeStyles } from 'tss-react/mui';
 
 function App() {
+  const useStyles = makeStyles()(() => ({
+    App: {
+      backgroundColor: "#14161a",
+      color: "white",
+      minHeight: "100vh",
+    }
+  }))
+
+  const {classes} = useStyles()
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <div className={classes.App}>
+        <div>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Homepage/>} exact />
+            <Route path='/coin/:id' element={<Coin/>} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 

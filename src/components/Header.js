4441c06ffc5a8452d895709e0b741f324b7004d9
daudi13 <davouma13@gmail.com>
2022-11-16 14:25:51 +0300

    Add header elements

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,50 @@
+import { AppBar, Toolbar, Typography, Select, MenuItem, createTheme, ThemeProvider } from '@mui/material'
+import { Container } from '@mui/system'
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import { makeStyles } from 'tss-react/mui';
 
 const Header = () => {
+  const useStyles = makeStyles()(() => ({
+    title: {
+      flex: 1,
+      color: "gold",
+      fontFamily: "Montserrat",
+      fontWeight: "bold",
+      cursor: "pointer",
+    }
+  }))
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    }
+  });
+
+  const { classes } = useStyles();
+  const navigate = useNavigate()
+
   return (
-    <div>
-      <p>Header</p>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color='transparent' position='static'>
+        <Container>
+          <Toolbar>
+            <Typography className={classes.title} onClick={() => navigate("/")}>Crypto Watchman</Typography>
+            <Select variant='outlined' style={
+              {
+                width: 100,
+                height: 40,
+                marginLeft: 15,
+                color: "white"
+              }
+            }>
+              <MenuItem value={'ksh'}>Ksh</MenuItem>
+              <MenuItem value={'tsh'}>Tsh</MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
   )
 }
 

6a73c3d79acdd8944f092c3485e8b17cb36a9bdb
daudi13 <davouma13@gmail.com>
2022-11-18 11:05:30 +0300

    Add fetchCoins functions to collect coins from Api

--- src/components/CoinsTable.js
+++ src/components/CoinsTable.js
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { CoinList } from '../config/api';
+import { CryptoState } from '../CryptoContext';
+import axios from 'axios';
+import { ThemeProvider, createTheme, Container, Typography } from '@mui/material';
 
 function CoinsTable() {
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const { currency } = CryptoState();
+
+  const fetchCoins = async () => {
+    setLoading(true)
+    const { data } = await axios.get(CoinList(currency))
+    setCoins(data);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchCoins();
+    // eslint-disable-next-line
+  }, [currency])
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark'
+    }
+  })
+  
+  console.log(coins)
+
   return (
-    <div>CoinsTable</div>
+    <ThemeProvider theme={darkTheme}>
+      <Container style={{ textAlign: "center" }}>
+        <Typography
+          variant="h4"
+          style={{margin: 18, fontFamily: "Montserrat"}}
+        >
+          Cryptocurrency Prices by Market cap
+        </Typography>
+      </Container>
+    </ThemeProvider>
   )
 }
 

0c7624178a9aab968feca8fbe1b3632091a26111
daudi13 <davouma13@gmail.com>
2022-11-21 17:32:18 +0300

    Add inital code setup for coinChart.js

--- src/components/CoinChart.js
+++ src/components/CoinChart.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import axios from 'axios';
+import React, {useEffect, useState} from 'react'
+import { CryptoState } from '../CryptoContext';
+import { HistoricalChart } from '../config/api';
 
-const CoinChart = () => {
+const CoinChart = ({coin}) => {
+  const [historicalData, setHistoricalData] = useState();
+  const [days, setDays] = useState();
+
+  const { currency } = CryptoState()
+  
+  const fetchHistoricalData = async () => {
+    const { data } = await axios.get(HistoricalChart(coin.id, days, currency));
+    setHistoricalData(data.prices)
+  }
+
+  console.log(historicalData)
+
+  useEffect(() => {
+    fetchHistoricalData();
+  }, [currency, days])
+
   return (
-    <div>coin info</div>
+    <div></div>
   )
 }
 

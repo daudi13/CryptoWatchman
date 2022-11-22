c94f68efd542377382a6a0eabe4bed83659d584a
daudi13 <davouma13@gmail.com>
2022-11-22 17:29:42 +0300

    Delete commented code

--- src/components/CoinChart.js
+++ src/components/CoinChart.js
@@ -79,18 +79,6 @@ const CoinChart = ({coin}) => {
         width: "100%"
       }
     },
-    // btn: {
-    //   border: "1px solid gold",
-    //   borderRadius: 5,
-    //   padding: 10,
-    //   paddingLeft: 20,
-    //   paddingRight: 20,
-    //   fontFamily: "Montserrat",
-    //   cursor: "pointer",
-    //   backgroundColor: selected ? "gold" : "",
-    //   color: selected ? "black" : "",
-    //   fontWeight: selected ? 700 : 500
-    // }
   }))
 
   const { classes } = useStyles();

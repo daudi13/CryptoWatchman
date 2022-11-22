484b164a4c913c22df4c9e5a412f85a506838e9e
daudi13 <davouma13@gmail.com>
2022-11-22 11:59:22 +0300

    Add chart graph components

--- src/components/CoinChart.js
+++ src/components/CoinChart.js
@@ -5,11 +5,32 @@ import { HistoricalChart } from '../config/api';
 import { createTheme, ThemeProvider } from '@mui/system';
 import { makeStyles } from 'tss-react/mui';
 import { Line } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
 
 
 const CoinChart = ({coin}) => {
   const [historicalData, setHistoricalData] = useState();
-  const [days, setDays] = useState();
+  const [days, setDays] = useState(1);
 
   const { currency } = CryptoState()
   
@@ -56,11 +77,21 @@ const CoinChart = ({coin}) => {
         {
           !historicalData ? ("Loading...") : (
             <Line
+              style={{height: "35vh"}}
               data={{
                 labels: historicalData.map((coin) => {
                   let date = new Date(coin[0]);
                   let time = date.getHours() > 12 ? `${date.getMinutes() - 12}: ${date.getMinutes()} PM` : `${date.getHours()}: ${date.getMinutes()} AM`
-                })
+
+                  return days === 1 ? time : date.toLocaleDateString()
+                }),
+                datasets: [
+                  {
+                    data: historicalData.map((coin) => coin[1]),
+                    label: `Price ( Past ${days} Day${days > 1 ? "s" : ''}) in ${currency}`,
+                    borderColor: "gold"
+                  }
+                ]
               }}
             />)
         }

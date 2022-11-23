0611a949a2fa8729d1252ba3e2aaa28f07674939
daudi13 <davouma13@gmail.com>
2022-11-23 16:54:50 +0300

    Create firebaseConfig

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,11 @@
+// Import the functions you need from the SDKs you need
+const firebaseConfig = {
+  apiKey: "AIzaSyAEY0wi3NLRJPhesMbdzST2GjvO5s7qMiE",
+  authDomain: "cryptowatchman-3f66e.firebaseapp.com",
+  projectId: "cryptowatchman-3f66e",
+  storageBucket: "cryptowatchman-3f66e.appspot.com",
+  messagingSenderId: "1045511870334",
+  appId: "1:1045511870334:web:a5e33286e1bbeb1010052f"
+};
+
+export default firebaseConfig;

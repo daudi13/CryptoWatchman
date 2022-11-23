669de1411cb1253aabb800f36b15addc7fa197a8
daudi13 <davouma13@gmail.com>
2022-11-23 13:43:12 +0300

    Add Helmet to Homepage

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Banner from '../components/Banner/Banner'
 import CoinsTable from '../components/CoinsTable';
+import { Helmet } from 'react-helmet';
 
 const Homepage = () => {
   return (
     <>
+      <Helmet>
+        <meta charSet='utf-8'/>
+        <title>CryptoWatchMan | Welcome</title>
+      </Helmet>
       <Banner />
       <CoinsTable/>
     </>

d807ccd253220396173858fa5b2aa3e1a1de9571
daudi13 <davouma13@gmail.com>
2022-11-26 21:01:23 +0300

    Add modal component

--- src/components/Header.js
+++ src/components/Header.js
@@ -80,21 +80,6 @@ const Header = () => {
           </Toolbar>
         </Container>
       </AppBar>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
-      </Modal>
     </ThemeProvider>
   )
 }

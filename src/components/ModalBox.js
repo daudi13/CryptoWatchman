f7f65bd6fc03214d3c836aeed8c5ff06f7c78751
daudi13 <davouma13@gmail.com>
2022-11-28 16:59:37 +0300

    Add tabs

--- src/components/ModalBox.js
+++ src/components/ModalBox.js
@@ -1,25 +1,48 @@
 import React from 'react';
-import { Typography } from '@mui/material'
 import Modal from '@mui/material/Modal';
+import Tabs from '@mui/material/Tabs';
 import Box from '@mui/material/Box';
-import { CryptoState } from '../CryptoContext';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabPanel from '@mui/lab/TabPanel';
+import { makeStyles } from 'tss-react/mui';
+
+
 
 const style = {
   position: 'absolute',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 400,
+  width: 450,
   bgcolor: 'background.paper',
   border: '2px solid #000',
   boxShadow: 24,
   color: "#fff",
-  p: 4,
+  borderRadius: 4
 };
 
-function ModalBox({open, setOpen}) {
-  const handleClose = () => setOpen(false);
 
+
+function ModalBox({ open, setOpen }) {
+  const [value, setValue] = React.useState('1');
+  
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  const handleClose = () => setOpen(false);
+  
+  
+  const useStyles = makeStyles()(() => ({
+    tabs: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "space-between",
+      width: "100%"
+    }
+  }));
+
+  const { classes } = useStyles();
   return (
   <Modal
       open={open}
@@ -28,12 +51,16 @@ function ModalBox({open, setOpen}) {
       aria-describedby="modal-modal-description"
     >
       <Box sx={style}>
-        <Typography id="modal-modal-title" variant="h6" component="h2">
-          Text in a modal
-        </Typography>
-        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-          Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-        </Typography>
+      <TabContext value={value}>
+        <Box sx={{ width: '100%', bgcolor: 'grey', borderRadius: 3 }}>
+            <Tabs value={value} onChange={handleChange} textColor="seco" centered>
+        <Tab label="Login" value="1" />
+        <Tab label="Sign up" value="2"/>
+      </Tabs>
+        </Box>
+        <TabPanel value="1">Item One</TabPanel>
+        <TabPanel value="2">Item Two</TabPanel>
+      </TabContext>
       </Box>
   </Modal>
   )

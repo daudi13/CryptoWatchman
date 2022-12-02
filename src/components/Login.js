c4b2b8e5cbb86261932e3cae42b041bfd2290283
daudi13 <davouma13@gmail.com>
2022-12-02 13:51:38 +0300

    Add Login logic

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,37 @@
 import React, { useState } from 'react';
 import { TextField, Button } from '@mui/material';
 import { Box } from '@mui/system';
+import { auth } from '../firebase';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { CryptoState } from '../CryptoContext';
 
-function Login() {
+
+function Login({handleClose}) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const handleSubmit = () => console.log("submit");
+  const { setAlert } = CryptoState();
+
+  const handleSubmit = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user
+        console.log(user);
+        setAlert({
+          open: true,
+          message: "You've successfully logged in",
+          type: "success"
+        })
+        handleClose();
+      })
+      .catch((error) => {
+        setAlert({
+          open: true,
+          message: `${error.message}`,
+          type: "error"
+        })
+        handleClose();
+    })
+  };
 
   return (
     <Box p={3} style={{ display: "flex", flexDirection: "column", gap: "20px", alignItems: "center" }}>
